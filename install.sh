5f51f1589baa5df08f3cbb67469d61b412577834
Davide <rondey90@gmail.com>
2025-09-06 18:31:43 +0200

    Fixed installation on the frontend

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ git submodule init
 git submodule update
 
 docker compose run backend sh -c "yarn install && yarn migration:run && yarn seed:run"
-docker compose run frontend npm run build
+docker compose run frontend npm install
