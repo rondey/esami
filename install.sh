9f6fa1179cb69ac7ada7c7c72395b89ac192ea21
Davide <rondey90@gmail.com>
2025-09-06 18:15:19 +0200

    Added the migration and the seed

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 git submodule init
 git submodule update
 
-docker compose run backend yarn install
+docker compose run backend sh -c "yarn install && yarn migration:run && yarn seed:run"
 docker compose run frontend npm run build
